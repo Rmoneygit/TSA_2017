e4658a5630a70e56a330554613fb468f274c7fda
Ryan Patrick <rmoneymail5@gmail.com>
2017-01-31 19:21:30 -0800

    Began project

---
+++ 2017 Project/Assets/TRIGGERED.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TRIGGERED : MonoBehaviour {
+
+    public void OnTriggerEnter()
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    public void OnTriggerExit()
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.white;
+    }
+}
