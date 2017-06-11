000cdba63eaab384deae7347b1449225be5824b4
Ryan Patrick <rmoneymail5@gmail.com>
2017-06-10 20:55:26 -0700

    I'm seeing what this looks like

---
+++ 2017 Project/Assets/Scripts/ChangeWallColor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeWallColor : MonoBehaviour {
+
+    public GameObject wall;
+
+    public void OnTriggerEnter()
+    {
+        wall.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    public void OnTriggerExit()
+    {
+        wall.GetComponent<Renderer>().material.color = Color.white;
+    }
+
+}
--- 2017 Project/Assets/Scripts/Rotate.cs
+++ 2017 Project/Assets/Scripts/Rotate.cs
@@ -2,15 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChangeWallColor : MonoBehaviour {
+public class Rotate : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * 10);
 	}
 }
